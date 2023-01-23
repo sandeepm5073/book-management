806ce666fea89039d74ba9aafdb157ba42a89b16
gauravGo7 <grvgoswami2007@gmail.com>
2023-01-23 14:05:58 +0530

    project

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,9 +1,57 @@
 const { default: mongoose } = require("mongoose")
 const { object } = require("webidl-conversions")
 const BookModel = require("../models/bookModel")
+const UserModel = require("../Models/UserModel")
 
 
 
+
+const createBooks=async function(req,res){
+    try{
+    let data=req.body
+    const {title,excerpt,userId,ISBN ,category,subcategory,releasedAt}=data
+    if(data.length==0){
+        return res.status(400).send({status:false,msg:"please give some data to create a book"})
+    }
+    if(!title){
+        return res.status(400).send({status:false,msg:"title is mandatory"})
+    }
+    if(!excerpt){
+        return res.status(400).send({status:false,msg:"excerpt is mandatory"})
+    }
+    if(!userId){
+        return res.status(400).send({status:false,msg:"userId is mandatory"})
+    }
+    if(!ISBN){
+        return res.status(400).send({status:false,msg:"ISBN is mandatory"})
+    }
+    if(!category){
+        return res.status(400).send({status:false,msg:"category is mandatory"})
+    }
+    if(!subcategory){
+        return res.status(400).send({status:false,msg:"subcategory is mandatory"})
+    }
+
+    if(!releasedAt){
+        return res.status(400).send({status:false,msg:"releasedAt is mandatory"})
+    }
+
+     let verifyTitle=await BookModel.findOne({title:title})
+    if(verifyTitle){
+        return res.status(400).send({status:false,msg:"title already exists"})
+    }
+
+    let verifyISBN=await BookModel.findOne({ISBN:ISBN})
+    if(verifyISBN){
+        return res.status(400).send({status:false,msg:" ISBN already exists"})
+    }
+    const newBook=await BookModel.create(data)
+    return res.status(201).send({status:true,msg:"book created successfully",data:newBook})
+}
+catch(error){
+return res.status(500).send({status:false,msg:error.message})
+}
+}
 
 
 
@@ -43,3 +91,7 @@ const getBook = async (req, res) => {
       return res.status(200).send({status:true, message:"Book list", data:filterBook})
 }
 // _id, title, excerpt, userId, category, releasedAt, reviews field.
+
+module.exports.createBooks=createBooks
+
+module.exports.getBook=getBook
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const UserController=require("../controllers/userController")
+const bookController=require("../controllers/bookController")
 
+router.post ("/register",UserController.createUser)
+router.post ("/login",UserController.loginUser)
 
-
+router.post ("/books",bookController.createBooks)
+router.get ("/books",bookController.getBook)
 
 module.exports = router
